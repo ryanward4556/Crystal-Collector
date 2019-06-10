71f06534712a52a01c0616d40743c72f159adef3
Ryan Ward <ryanward4556@gmail.com>
2019-06-10 14:08:09 -0400

    wrote basic psuedocode for game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+//  Global Variables
+
+userGuess = "";
+userScore = 0;
+targetScore = 0;
+targetComplete = false;
+
+//  ----------------------------------------------------------------------------
+
+/*
+*   ----------------------------------------------------------------------------
+*   When page loads (document.ready)...
+*   ----------------------------------------------------------------------------
+*/
+
+//  Generate random targetScore between 19-120
+
+//  Generate crystal amounts, each to random
+
+/*
+*   ----------------------------------------------------------------------------
+*   When user clicks...
+*   ----------------------------------------------------------------------------
+*/
+
+
+//  Listen for user to click on one of the crystals
+
+//  Increase userScore by crystal clicked
+
+//  If statement: 
+//  if userScore === target Score, "You win!", call restart game function
+//  if userScore > targetScore, "You lose :(", call restart game function
+//  if userScore < targetScore, wait for another crystal to be clicked
+
+
+
+
+
+
